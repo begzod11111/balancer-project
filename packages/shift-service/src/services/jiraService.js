94b2b07097480a707ecb347ea18f462604f41f04
behzod <begzodbey0426@gmail.com>
2026-01-14 17:24:35 +0500

    jiraAxios add funk local to shift serves

--- packages/shift-service/src/services/jiraService.js
+++ packages/shift-service/src/services/jiraService.js
@@ -1,4 +1,21 @@
-import jiraAxios from '../../../../shared/jiraAxios.js';
+import axios from "axios";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+
+const jiraAxios = axios.create({
+    baseURL: `https://${process.env.JIRA_URL}/rest/api/3`,
+    auth: {
+        username: process.env.JIRA_USERNAME,
+        password: process.env.JIRA_API_TOKEN,
+    },
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+});
 
 
 
