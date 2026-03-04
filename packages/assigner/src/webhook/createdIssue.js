6b608bb91f48ae322f8a5bc7aa44138f1d8fb469
behzod <begzodbey0426@gmail.com>
2026-03-04 12:22:32 +0500

    подключил назначении

--- packages/assigner/src/webhook/createdIssue.js
+++ packages/assigner/src/webhook/createdIssue.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import publishIssueCreated from '../config/publishIssueCreated.js';
+import redisServices from "../services/redisServices.js";
+import assignedServices from "../services/assignedServices.js";
 
 const router = express.Router();
 
 router.post('/created-issue', async (req, res) => {
     try {
         const issueData = req.body;
-
+        const assignmentGroupId =  issueData.issue.fields.customfield_18219?.[0]?.objectId || null
         await publishIssueCreated(issueData);
-
+        await assignedServices.assignedIssue(issueData.issue.key, assignmentGroupId);
         res.status(200).json({success: true, message: 'Issue processed successfully'});
     } catch (error) {
         console.error('[Assigner] Error processing created issue webhook:', error);
