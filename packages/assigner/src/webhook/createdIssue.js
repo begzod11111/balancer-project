45c04cf3362a370386861f85ea94078ae102211f
behzod <begzodbey0426@gmail.com>
2026-03-16 12:27:42 +0500

    добавлен ожидания на 3 секунд

--- packages/assigner/src/webhook/createdIssue.js
+++ packages/assigner/src/webhook/createdIssue.js
@@ -10,6 +10,9 @@ router.post('/created-issue', async (req, res) => {
         const issueData = req.body;
         const assignmentGroupId =  issueData.issue.fields.customfield_18219?.[0]?.objectId || null
         await publishIssueCreated(issueData);
+
+        await new Promise(resolve => setTimeout(resolve, 30000));
+
         await assignedServices.assignedIssue(issueData.issue.key, assignmentGroupId);
         res.status(200).json({success: true, message: 'Issue processed successfully'});
     } catch (error) {
